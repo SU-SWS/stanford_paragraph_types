b82ffc9dd4c28572a8a85644c4ed11b71d039e72
pookmish <pookmish@stanford.edu>
2017-02-27 18:48:12 -0800

    HSDO-737 HSDO-696 Added radios and validation for hero type (#4)

--- js/stanford_paragraph_types.admin.js
+++ js/stanford_paragraph_types.admin.js
@@ -8,27 +8,54 @@
     attach: function (context, settings) {
 
       $('.group-p-card-cta', context).each(function () {
-        var group = this;
+        $group = this;
         $(this).find('.form-radio').each(function () {
           if ($(this).is(':checked')) {
-            teaserCardsCTA(group, $(this).val());
+            teaserCardsCTA($group, $(this).val());
           }
           $(this).change(function () {
-            teaserCardsCTA(group, $(this).val());
+            teaserCardsCTA($group, $(this).val());
           });
         });
 
       });
 
+
+      $('.field-name-field-p-hero-style', context).each(function () {
+        $style = $(this);
+
+        $(this).find('.form-radio').each(function () {
+          if ($(this).is(':checked')) {
+            heroStyle($style, $(this).val());
+          }
+          $(this).change(function () {
+            heroStyle($style, $(this).val());
+          });
+        });
+      });
+
+
       // Used this method vs conditional_fields since the contrib module failed to function correctly after a 2nd
       // item was added.
-      function teaserCardsCTA(group, radioVal) {
+      function teaserCardsCTA($group, radioVal) {
         if (radioVal == 'link') {
-          $(group).find('.field-name-field-p-card-cta').show();
-          $(group).find('.field-name-field-p-card-file').hide();
+          group.find('.field-name-field-p-card-cta').show();
+          group.find('.field-name-field-p-card-file').hide();
         } else {
-          $(group).find('.field-name-field-p-card-cta').hide();
-          $(group).find('.field-name-field-p-card-file').show();
+          group.find('.field-name-field-p-card-cta').hide();
+          group.find('.field-name-field-p-card-file').show();
+        }
+      }
+
+      function heroStyle($style, radioVal) {
+        if (radioVal == 'image') {
+          $style.siblings('.field-name-field-p-hero-image').show();
+          $style.siblings('.field-name-field-p-hero-video').hide();
+        } else if (radioVal == 'video') {
+          $style.siblings('.field-name-field-p-hero-image').hide();
+          $style.siblings('.field-name-field-p-hero-video').show();
+        } else {
+          $style.siblings().show();
         }
       }
     }
