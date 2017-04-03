0d1370404c78a2fc0b227ee4e9d4220052636be9
Mike Decker <pookmish@gmail.com>
2017-04-03 15:50:05 -0700

    HSDO-772 Add color option to callout text paragraph

--- theme/paragraphs-item--p-callout.tpl.php
+++ theme/paragraphs-item--p-callout.tpl.php
@@ -5,9 +5,10 @@
  * P_callout paragraph type.
  */
 ?>
-
-<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="content row-fluid"<?php print $content_attributes; ?>>
-    <?php print render($content); ?>
+<div <?php print $attributes; ?>>
+  <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <div class="content row-fluid"<?php print $content_attributes; ?>>
+      <?php print render($content); ?>
+    </div>
   </div>
 </div>
