73cf0d7a263013fa5d9f5bc34b0887269c88daf6
Mike Decker <pookmish@gmail.com>
2017-04-27 07:45:23 -1000

    Hero overlay use keyboard to go to video url page

--- js/stanford_paragraph_types.js
+++ js/stanford_paragraph_types.js
@@ -7,25 +7,29 @@
   Drupal.behaviors.stanfordParagraphTypes = {
     attach: function (context, settings) {
       $('.field-name-field-p-hero-image', context).each(function () {
-        if ($(this).siblings('.field-name-field-p-hero-video').length) {
+        var video = $(this).siblings('.field-name-field-p-hero-video');
+
+        if (video.length) {
+          var videoUrl = $(video).find('div[data-video-embed-url]').attr('data-video-embed-url');
+
           var play = $('<a>', {
             class: 'play-video',
-            href: '#',
-            html: $('<i>', {class: 'fa fa-youtube-play icon-youtube-play', html: 'Play'})
-          }).click(function (e) {
+            href: videoUrl,
+            html: $('<i>', {class: 'fa fa-youtube-play icon-youtube-play', html: 'Play Video'})
+          }).mouseup(function (e) {
             e.preventDefault();
 
             $dad = $(this).parent();
             $dad.hide();
             $dad.siblings('.group-overlay-text').hide();
-            var video = $dad.siblings('.field-name-field-p-hero-video');
-            var iframe = $dad.siblings('.field-name-field-p-hero-video').find('iframe')[0];
+            var iframe = $(video).find('iframe')[0];
 
             iframe.src += "&autoplay=1";
             $(iframe).attr('onload', 'this.contentWindow.focus()');
             $(video).show();
 
           });
+
           $(this).prepend(play);
         }
       });
