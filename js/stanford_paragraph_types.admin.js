7109c08803f1a7865bc22ffd358afb0d22151582
pookmish <pookmish@stanford.edu>
2017-10-12 10:44:51 -0700

    Fixed icon paragraph when using images. also improved scss (#91)
    * Fixed icon paragraph when using images. also improved scss
    
    * changed 'und' to LANGUAGE_NONE

--- js/stanford_paragraph_types.admin.js
+++ js/stanford_paragraph_types.admin.js
@@ -8,7 +8,7 @@
     attach: function (context, settings) {
 
       $('.field-name-field-p-icon-color option', context).each(function () {
-        $(this).addClass('color-'+ $(this).attr('value'));
+        $(this).addClass('color-' + $(this).attr('value'));
       });
 
       $('.group-p-card-cta', context).each(function () {
@@ -36,13 +36,14 @@
         });
       });
 
-      // Used this method vs conditional_fields since the contrib module failed to function correctly after a 2nd
-      // item was added.
+      // Used this method vs conditional_fields since the contrib module failed
+      // to function correctly after a 2nd item was added.
       function teaserCardsCTA(group, radioVal) {
         if (radioVal == 'link') {
           $(group).find('.field-name-field-p-card-cta').show();
           $(group).find('.field-name-field-p-card-file').hide();
-        } else {
+        }
+        else {
           $(group).find('.field-name-field-p-card-cta').hide();
           $(group).find('.field-name-field-p-card-file').show();
         }
@@ -52,13 +53,52 @@
         if (radioVal == 'image') {
           $style.siblings('.field-name-field-p-hero-image').show();
           $style.siblings('.field-name-field-p-hero-video').hide();
-        } else if (radioVal == 'video') {
+        }
+        else if (radioVal == 'video') {
           $style.siblings('.field-name-field-p-hero-image').hide();
           $style.siblings('.field-name-field-p-hero-video').show();
-        } else {
+        }
+        else {
           $style.siblings().show();
         }
       }
+
+      $('.field-name-field-p-icon-icon-style input', context).change(function () {
+        var fieldset = $(this).closest('fieldset');
+        pIconStyleHideShow(fieldset, $(this).val());
+      });
+
+      function pIconGridStyle() {
+        $('.field-name-field-p-icon-icon-style input:checked').each(function () {
+          pIconStyleHideShow($(this).closest('fieldset'), $(this).val());
+        });
+      }
+
+      function pIconStyleHideShow(fieldset, value) {
+        var showFields = ['.field-name-field-p-icon-icon-style'];
+        var hideFields = [];
+
+        switch (value) {
+          case 'icon':
+            showFields.push('.field-name-field-p-icon-icon');
+            hideFields.push('.field-name-field-p-icon-image');
+            break;
+
+          case 'image':
+            showFields.push('.field-name-field-p-icon-image');
+            hideFields.push('.field-name-field-p-icon-icon');
+            break;
+
+        }
+        $.each(showFields, function (i, field) {
+          $(fieldset).find(field).show().find('select').trigger('chosen:updated');
+        });
+        $.each(hideFields, function (i, field) {
+          $(fieldset).find(field).hide();
+        });
+      }
+
+      pIconGridStyle();
     }
   }
 })(jQuery);
