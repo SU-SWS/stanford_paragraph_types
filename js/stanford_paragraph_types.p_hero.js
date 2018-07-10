133067eb693312f84b56cef2f2338a3a36312adb
Mike Decker <pookmish@gmail.com>
2018-07-10 08:34:16 -0700

    added vimeo support

--- js/stanford_paragraph_types.p_hero.js
+++ js/stanford_paragraph_types.p_hero.js
@@ -3,11 +3,9 @@
  * Applies some functionality on the paragraph types.
  */
 
-var tag = document.createElement('script');
-tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
+/**
+ * Called automatically by YouTube's API.
+ */
 function onYouTubeIframeAPIReady() {
   // After Youtube API is loaded, we can do what we need.
   Drupal.behaviors.stanfordParagraphPHero.youTubeReady()
@@ -15,65 +13,121 @@ function onYouTubeIframeAPIReady() {
 
 (function ($) {
   Drupal.behaviors.stanfordParagraphPHero = {
-    youTubeReady: function () {
+    heroFunctions: this,
 
+    attach: function (context, settings) {
       var i = 0;
+      $('.field-name-field-p-hero-video iframe').each(function () {
+        // Make sure the video has an ID attribute.
+        if (typeof $(this).attr('id') == 'undefined') {
+          $(this).attr('id', 'hero-video-' + i);
+        }
+        i++;
+      });
+
+
+      var tag = document.createElement('script');
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+
+      if ($('.field-name-field-p-hero-video iframe[src*="youtube"]').length) {
+        tag.src = '//www.youtube.com/iframe_api';
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      }
+
+      if ($('.field-name-field-p-hero-video iframe[src*="vimeo"]').length) {
+        $.getScript('//player.vimeo.com/api/player.js', function () {
+          Drupal.behaviors.stanfordParagraphPHero.vimeoReady();
+        });
+      }
+
+    },
+
+    youTubeReady: function () {
+      $heroFunctions = this;
 
       $('.field-name-field-p-hero-image').once('youtube', function () {
-        var $video = $(this).siblings('.field-name-field-p-hero-video').find('iframe');
+        var $video = $(this).siblings('.field-name-field-p-hero-video').find('iframe[src*="youtube"]');
 
         if (!$video.length) {
           return;
         }
-
-        // Gives the video unique ID's and a title.
-        $video.attr('id', 'hero-video-' + i)
-          .attr('onload', 'this.contentWindow.focus()')
+        $video.attr('onload', 'this.contentWindow.focus()')
           .attr('title', Drupal.t('Video Player'));
 
         // Initialze the Youtube api.
-        new YT.Player($video.attr('id'), {
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-        i++;
+        new YT.Player($video.attr('id'), {events: {'onReady': onYoutubePlayerReady}});
       });
 
-      function onPlayerReady(event) {
-        var fieldWrapper = event.target.a.closest('.field-name-field-p-hero-video');
-        var imageOverlay = $(fieldWrapper).siblings('.field-name-field-p-hero-image');
+      /**
+       * When the youtube video is ready to play, we'll add a play button over
+       * the image and add the click listener.
+       *
+       * @param event
+       */
+      function onYoutubePlayerReady(event) {
+        $fieldWrapper = $(event.target.a.closest('.field-name-field-p-hero-video'));
+        $imageOverlay = $fieldWrapper.siblings('.field-name-field-p-hero-image');
 
-        var play = $('<a>', {
-          class: 'play-video',
-          href: event.target.getVideoUrl(),
-          html: $('<i>', {
-            class: 'fa fa-youtube-play icon-youtube-play',
-            html: 'Play Video',
-            'aria-label': Drupal.t('Play Video - Opens to the video website')
-          })
-        }).click(function (e) {
+        var play = $heroFunctions.getPlayerButton(event.target.getVideoUrl());
+        play.click(function (e) {
           // Mouse has eventPhase 3, keyboard has 2.
           if (e.eventPhase == 3) {
             e.preventDefault();
             $dad = $(this).parent();
             $dad.hide();
             $dad.siblings('.group-overlay-text').hide();
-            // $(iframe).attr('onload', 'this.contentWindow.focus()');
             event.target.playVideo();
-            $(fieldWrapper).show();
+            $fieldWrapper.show();
           }
         });
 
-        $(imageOverlay).prepend(play);
-
-
+        $imageOverlay.prepend(play);
       }
+    },
 
-      function onPlayerStateChange(event) {
-        // changeBorderColor(event.data);
-      }
+    vimeoReady: function () {
+      $('.field-name-field-p-hero-video iframe[src*="vimeo"]').each(function () {
+        $video = $(this);
+        var player = new Vimeo.Player($video);
+
+        $fieldWrapper = $($video.closest('.field-name-field-p-hero-video'));
+        $imageOverlay = $fieldWrapper.siblings('.field-name-field-p-hero-image');
+
+        var play = Drupal.behaviors.stanfordParagraphPHero.getPlayerButton($video.attr('src'));
+
+        play.click(function (e) {
+          // Mouse has eventPhase 3, keyboard has 2.
+          if (e.eventPhase == 3) {
+            e.preventDefault();
+            $dad = $(this).parent();
+            $dad.hide();
+            $dad.siblings('.group-overlay-text').hide();
+            $fieldWrapper.show();
+            player.play();
+          }
+        });
+
+        $imageOverlay.prepend(play);
+      });
+    },
+
+    /**
+     * Get a generic play button link.
+     *
+     * @param hrefUrl
+     * @returns {*|HTMLElement}
+     */
+    getPlayerButton: function (hrefUrl) {
+      return $('<a>', {
+        class: 'play-video',
+        href: hrefUrl,
+        html: $('<i>', {
+          class: 'fa fa-youtube-play icon-youtube-play',
+          html: 'Play Video',
+          'aria-label': Drupal.t('Play Video - Opens to the video website')
+        })
+      });
     }
+
   }
 })(jQuery);
