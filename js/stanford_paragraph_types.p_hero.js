039a692a8b5a7483db31447ca525237bc0aed032
pookmish <pookmish@stanford.edu>
2018-10-16 15:07:30 -0700

    Jump 2923 vidoe autoplay and options (#103)
    * JUMP-2923 Allowed parameters in url to take priority
    * SITES-990: use youtubes api to play the video
    * SITES-990: added vimeo support
    * SITES-990: Add autoplay handling to click action.

--- js/stanford_paragraph_types.p_hero.js
+++ js/stanford_paragraph_types.p_hero.js
@@ -5,42 +5,162 @@
 
 (function ($) {
   Drupal.behaviors.stanfordParagraphPHero = {
+
     attach: function (context, settings) {
-      $('.field-name-field-p-hero-image', context).each(function () {
-        var video = $(this).siblings('.field-name-field-p-hero-video');
 
-        if (video.length) {
-          $(video).find('iframe').attr('title', Drupal.t('Video Player'));
-          var videoUrl = $(video).find('div[data-video-embed-url]').attr('data-video-embed-url');
-
-          var play = $('<a>', {
-            class: 'play-video',
-            href: videoUrl,
-            html: $('<i>', {
-              class: 'fa fa-youtube-play icon-youtube-play',
-              html: 'Play Video',
-              'aria-label': Drupal.t('Play Video - Opens to the video website')
-            })
-          }).click(function (e) {
-
-            // Mouse has eventPhase 3, keyboard has 2.
-            if (e.eventPhase == 3) {
-              e.preventDefault();
-              $dad = $(this).parent();
-              $dad.hide();
-              $dad.siblings('.group-overlay-text').hide();
-              var iframe = $(video).find('iframe')[0];
-
-              iframe.src += "&autoplay=1";
-              $(iframe).attr('onload', 'this.contentWindow.focus()');
-              $(video).show();
-            }
-
-          });
-
-          $(this).prepend(play);
+      // Loop through each hero video fields and add a unique ID.
+      $('.field-name-field-p-hero-video iframe', context).each(function (i, iframe) {
+        // Make sure the video has an ID attribute.
+        if (typeof $(iframe).attr('id') == 'undefined') {
+          $(this).attr('id', 'hero-video-' + i);
         }
       });
+
+      // If there is a youtube player, add the player api script.
+      if ($('.field-name-field-p-hero-video iframe[src*="youtube"]', context).length) {
+        $.getScript('https://www.youtube.com/iframe_api');
+      }
+
+      // If there is a vimeo video add the player api script.
+      if ($('.field-name-field-p-hero-video iframe[src*="vimeo"]', context).length) {
+        $.getScript('https://player.vimeo.com/api/player.js', function () {
+          Drupal.behaviors.stanfordParagraphPHero.vimeoReady();
+        });
+      }
+    },
+
+    /**
+     * Called after YouTubes API is ready and adds event reactions to videos.
+     */
+    youTubeReady: function (context) {
+      // For each of the hero images, but only once, add onload/onready events
+      // to its sibling video player.
+      $('.field-name-field-p-hero-image', context).once('youtube', function (i, heroImage) {
+
+        // Look for a hero video.
+        var $video = $(heroImage).siblings('.field-name-field-p-hero-video').find('iframe[src*="youtube"]');
+
+        // End if none. The user might have only wanted an image, or it might
+        // be a vimeo video.
+        if (!$video.length) {
+          return;
+        }
+
+        // On each of the video elements attach an onReady event and set the
+        // title for accessibility reasons.
+        $video.attr('onload', 'this.contentWindow.focus()')
+          .attr('title', Drupal.t('Video Player'));
+
+        // Initialze the Youtube api.
+        new YT.Player($video.attr('id'), {
+          events: {
+            'onReady': onYoutubePlayerReady
+          }
+        });
+        // Force the iframe to allow the api event listeners.
+        $video.attr('src', $video.attr('src') + '&enablejsapi=1');
+      });
+
+
+      /**
+       * When the youtube video is ready to play, we'll add a play button over
+       * the image and add the click listener.
+       *
+       * @param event
+       */
+      function onYoutubePlayerReady(event) {
+        $fieldWrapper = $(event.target.a.closest('.field-name-field-p-hero-video'));
+        $imageOverlay = $fieldWrapper.siblings('.field-name-field-p-hero-image');
+
+        var $play = Drupal.behaviors.stanfordParagraphPHero.getPlayerButton(event.target.getVideoUrl());
+
+        // Add a click event to hide the hero image and play the video.
+        play.click(function (e) {
+          // Mouse has eventPhase 3, keyboard has 2. we only add the mouse
+          // event so that a keyboard will navigate the user to the respective
+          // YouTube page.
+          if (e.eventPhase == 3) {
+            e.preventDefault();
+
+            // Dad is the hero image.
+            $dad = $(this).parent();
+            $dad.hide();
+            $dad.siblings('.group-overlay-text').hide();
+
+            // Play and show the video.
+            $fieldWrapper.show();
+            event.target.playVideo();
+          }
+        });
+
+        // Prepend the play button to the DOM.
+        $imageOverlay.prepend($play);
+      }
+    },
+
+    /**
+     * After vimeo API is loaded, add the play button and listener to iframes.
+     */
+    vimeoReady: function (context) {
+      $('.field-name-field-p-hero-video iframe[src*="vimeo"]', context).each(function (i, iframe) {
+        var player = new Vimeo.Player($(iframe));
+
+        $fieldWrapper = $($(iframe).closest('.field-name-field-p-hero-video'));
+        $imageOverlay = $fieldWrapper.siblings('.field-name-field-p-hero-image');
+
+        var $play = Drupal.behaviors.stanfordParagraphPHero.getPlayerButton($(iframe).attr('src'));
+
+        play.click(function (e) {
+          // Mouse has eventPhase 3, keyboard has 2. we only add the mouse
+          // event so that a keyboard will navigate the user to the respective
+          // Vimeo page.
+          if (e.eventPhase == 3) {
+            e.preventDefault();
+
+            // Dad is the hero image.
+            $dad = $(this).parent();
+            $dad.hide();
+            $dad.siblings('.group-overlay-text').hide();
+
+            // Show and play the video.
+            $fieldWrapper.show();
+            player.play();
+          }
+        });
+
+        // Prepend the play button to the DOM.
+        $imageOverlay.prepend($play);
+      });
+    },
+
+    /**
+     * Get a generic play button link.
+     *
+     * @param hrefUrl
+     *   The url of the given video iframe.
+     * @returns {*|HTMLElement}
+     *   The A tag element.
+     */
+    getPlayerButton: function (hrefUrl) {
+      return $('<a>', {
+        class: 'play-video',
+        href: hrefUrl,
+        title: Drupal.t('Play Video'),
+        html: $('<i>', {
+          class: 'fa fa-youtube-play icon-youtube-play',
+          html: 'Play Video',
+          'aria-label': Drupal.t('Play Video - Opens to the video website')
+        })
+      });
     }
+
   }
 })(jQuery);
+
+/**
+ * Called automatically by YouTube's API.
+ */
+function onYouTubeIframeAPIReady() {
+  // After Youtube API is loaded, we can do what we need.
+  Drupal.behaviors.stanfordParagraphPHero.youTubeReady();
+}
